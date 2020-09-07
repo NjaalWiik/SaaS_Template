8608491aaa30c7454a47940a1b7710c70b91e274
Njaal Wiik <njaal.wiik@gmail.com>
2020-09-07 10:39:17 +0200

    Revert "register user and validate if user already exists"
    This reverts commit d0f7884bc7a654364a8e3c3d9b569e42c83c96b1.

--- server/middleware/authController.js
+++ server/middleware/authController.js
@@ -1,8 +1,6 @@
-const User = require('../models/User');
-
 const { restart } = require('nodemon');
 
-exports.register = async (req, res, next) => {
+exports.register = (req, res, next) => {
   const { email, password } = req.body;
 
   // Validate the input fields
@@ -41,41 +39,7 @@ exports.register = async (req, res, next) => {
     res.send(errorObject);
   }
 
-  // Save this info to DB
-  try {
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-      const errorObject = {
-        error: true,
-        errors: [
-          {
-            code: 'VALIDATION_ERROR',
-            field: 'email',
-            message: 'Email already exists'
-          }
-        ]
-      };
-
-      res.status(422).send(errorObject);
-      return;
-    }
-
-    let user = new User({
-      email,
-      password
-    });
-
-    const savedUser = await user.save();
-
-    console.log('savedUser', savedUser);
-
-    res.status(200).send({
-      user: savedUser
-    });
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server error');
-  }
+  res.send('success');
 };
 
 function validateEmail(email) {
--- server/models/User.js
+++ server/models/User.js
@@ -1,21 +1,25 @@
 const mongoose = require('mongoose');
 
-const UserSchema = new mongoose.Schema(
-  {
-    email: {
-      type: String,
-      required: true,
-      lowercase: true,
-      unique: true
+let User;
+
+if (!User) {
+  const UserSchema = new mongoose.Schema(
+    {
+      user: {
+        type: String,
+        required: true,
+        lowercase: true,
+        unique: true
+      },
+      password: {
+        type: String,
+        required: true
+      }
     },
-    password: {
-      type: String,
-      required: true
+    {
+      timestamps: true
     }
-  },
-  {
-    timestamps: true
-  }
-);
+  );
+}
 
 module.exports = User = mongoose.model('user', UserSchema);
